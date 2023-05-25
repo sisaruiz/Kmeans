ab147f0bd7a8b3ba78333b881e7be474766446b2
Sisa Ruiz <sisaruizm@gmail.com>
2023-05-25 17:53:15 +0200

    implement combiner

--- src/main/java/it/unipi/kurapika/KmeansCombiner.java
+++ src/main/java/it/unipi/kurapika/KmeansCombiner.java
@@ -1,5 +1,20 @@
 package it.unipi.kurapika;
 
-public class KmeansCombiner {
+import org.apache.hadoop.mapreduce.Reducer;
+import it.unipi.kurapika.utilities.*;
 
+import java.io.IOException;
+
+public class KmeansCombiner extends Reducer<Centroid, Point, Centroid, Point>{
+
+	@Override
+    protected void reduce(Centroid key, Iterable<Point> points, Context context) throws IOException, InterruptedException {
+        Point result = new Point();
+        
+        for (Point point : points) {
+            result.sum(point);
+        }
+        
+        context.write(key, result);
+	}
 }

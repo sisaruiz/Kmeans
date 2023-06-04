c8eb0b6ff4f57a948fa799a1294eb8cbb377301b
Sisa Ruiz <sisaruizm@gmail.com>
2023-06-04 11:15:07 +0200

    update mapper

--- kmeans/src/main/java/it/unipi/kurapika/KmeansMapper.java
+++ kmeans/src/main/java/it/unipi/kurapika/KmeansMapper.java
@@ -2,13 +2,10 @@ package it.unipi.kurapika;
 
 import java.io.IOException;
 import java.util.List;
-import java.lang.*;
 import java.util.ArrayList;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -25,7 +22,7 @@ public class KmeansMapper extends Mapper<LongWritable, Text, Centroid, Point>{
 		
 		super.setup(context);
 		Configuration conf = context.getConfiguration();
-		String[] lines = context.getConfiguration().getStrings("centroids");
+		String[] lines = conf.getStrings("centroids");
         
         // get centroids' values
         for(int i = 0; i < lines.length; i++)
@@ -43,9 +40,9 @@ public class KmeansMapper extends Mapper<LongWritable, Text, Centroid, Point>{
 		
 		for(int i=0; i<centroids.size(); i++) {									// for each centroid
 			double distance = point.getDistance(centroids.get(i).getPoint());	// calculate distance from point to centroid
-			if (distance < minimumDistance) {	// if distance is shorter than previous ones
-                	cluster = centroids.get(i);	// assign new centroid to point
-                	minimumDistance = distance;	// update minimumDistance with new distance
+			if (distance < minimumDistance) {		// if distance is shorter than previous ones
+                	cluster = centroids.get(i);		// assign new centroid to point
+                	minimumDistance = distance;		// update minimumDistance with new distance
             }
 		}
 		
